6fee09374f2a1adc08697cd2b411d740d58c6780
kodeFant <lars.lillo@gmail.com>
2018-09-20 16:44:23 +0200

    Optimize WPML scripts for PageSpeed

--- kodefantno/web/app/themes/kodefant/resources/functions.php
+++ kodefantno/web/app/themes/kodefant/resources/functions.php
@@ -184,3 +184,7 @@ function posts_ingress()
 {
     return get_field('posts_ingress');
 }
+
+define('ICL_DONT_LOAD_NAVIGATION_CSS', true);
+define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
+define('ICL_DONT_LOAD_LANGUAGES_JS', true);
