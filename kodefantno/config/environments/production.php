cecdc1c563246e2ac2765dc7e20ddd4efb20538d
kodeFant <lars.lillo@gmail.com>
2018-09-20 16:04:48 +0200

    Attempting to raise memory limit

--- kodefantno/config/environments/production.php
+++ kodefantno/config/environments/production.php
@@ -5,4 +5,4 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
-define('WP_MEMORY_LIMIT', '256');
+define('WP_MEMORY_LIMIT', '256M');
